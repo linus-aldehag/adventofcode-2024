04dc380ca30c4d54a2218a3751f976476d91c39e
linus.aldehag <linus.aldehag@tofindout.se>
2025-01-08 10:58:16 +0100

    Getting there step by step

--- Challenge02.py
+++ Challenge02.py
@@ -1,64 +1,54 @@
 from ChallengeBase import ChallengeBase
+def process_report(levels):
+  direction = 'unknown'
+  current_level = int(levels[0])
+
+  for index in range(1, len(levels)):
+    last_level = current_level
+    current_level = int(levels[index])
+
+    if current_level == last_level:
+      return index
+    elif current_level > last_level:
+      if current_level - last_level > 3:
+        return index
+    elif direction == 'unknown':
+      direction = 'up'
+    elif direction == 'down':
+      return -1
+    elif current_level < last_level:
+      if last_level - current_level > 3:
+        return -1
+    elif direction == 'unknown':
+      direction = 'down'
+    elif direction == 'up':
+      return -1
 
 challenge = ChallengeBase(2)
 
 data = challenge.load_data()
 
 safeReports = 0
-for report in data:
-  levels = report.split()
-  direction = 'unknown'
-  dampener_available = False
-  
-  current_level = int(levels[0])
-
-  for index in range(1, len(levels)):
-    lastLevel = current_level
-    current_level = int(levels[index])
-
-    if current_level == lastLevel:
-        if dampener_available:
-          dampener_available = False
-        else:
-            break
-    elif current_level > lastLevel:
-      if current_level - lastLevel > 3:
-        if dampener_available:
-          dampener_available = False
-        else:
-          break
-      elif direction == 'unknown':
-        direction = 'up'
-      elif direction == 'down':
-        if dampener_available:
-          dampener_available = False
-        else:
-          break
-    elif current_level < lastLevel:
-      if lastLevel - current_level > 3:
-        if dampener_available:
-          dampener_available = False
-        else:
-            break
-      elif direction == 'unknown':
-        direction = 'down'
-      elif direction == 'up':
-        if dampener_available:
-          dampener_available = False
-        else:
-            break
-
-    if index == len(levels) - 1:
+for reportRow in data:
+    levelData = reportRow.split()
+    dampenerUsed = False
+    result = process_report(levelData)
+    if result != -1:
       safeReports += 1
+    else:
+      if not dampenerUsed:
+        dampenerUsed = True
+        correctedRow = levelData.pop(result)
+        result = process_report(levelData)
+        if result != -1:
+          safeReports += 1
 
 def part_one():
-  print(safeReports)
+  print(safeReports) #incorrect
 
 def part_two():
-  print("Part Two Not Implemented")
+  print(safeReports)
 
-#more than 503
-#less than 521
 
 part_one()
 part_two()
